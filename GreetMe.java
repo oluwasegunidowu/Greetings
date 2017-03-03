aa89e66494244195a3eb32cbb8fa2528800f117e
oluwasegunidowu <stackzconceptz@gmail.com>
2017-03-03 20:07:12 +0100

    Add existing file

--- GreetMe.java
+++ GreetMe.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
  {
 
 	public static void main(String[] args) 
-	{
-		
+	{	
 		Scanner stdin = new Scanner( System.in );
-		
-		 String usersName; 
-		  String upperCaseName;
-	        
-		   System.out.print("Please enter your name: ");
-	            usersName = stdin.nextLine();
-		
-		     upperCaseName = usersName.toUpperCase();
-		
-		      System.out.println("Hi " + upperCaseName +","
-		       + " its nice to meet you on this beautiful day." );
+        int kayode = 1;
+        int segun = 2;
+        char name = 0;
+		String usersName; 
+		String upperCaseName;
 
-	}
-
-}
+	    System.out.print("Please enter your name: " + name);
+	    usersName = stdin.nextLine();
+	    upperCaseName = usersName.toUpperCase();
+	    if (name ==1 );
+	    { 
+	       System.out.println("Hi, "+ upperCaseName + ", its nice to meet you on this beautiful day." );
+	    } 
+	    else if (name==2)
+	    {
+	       System.out.println("Hi, "+ upperCaseName + ",its nice to meet you on this beautiful day." );
+		}
+        else 
+        { 
+	       System.out.println("Hi,its nice to meet you on this beautiful day." );
+        }
+	       
+    } 
+ }
