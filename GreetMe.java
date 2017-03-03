bb9ff294db334f64553c1ae9addc80731aac98f4
Oluwasegun Orisetimehin Idowu <stackzconceptz@gmail.com>
2017-03-03 20:15:48 +0100

    Update GreetMe.java

--- GreetMe.java
+++ GreetMe.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 
 	public static void main(String[] args) 
 	{	
-		Scanner stdin = new Scanner( System.in );
-        int kayode = 1;
-        int segun = 2;
-        char name = 0;
+	    Scanner stdin = new Scanner( System.in );
+                int kayode = 1;
+                int segun = 2;
+                char name = 0;
 		String usersName; 
 		String upperCaseName;
 
@@ -16,17 +16,17 @@ import java.util.Scanner;
 	    usersName = stdin.nextLine();
 	    upperCaseName = usersName.toUpperCase();
 	    if (name ==1 );
-	    { 
+	        { 
 	       System.out.println("Hi, "+ upperCaseName + ", its nice to meet you on this beautiful day." );
-	    } 
-	    else if (name==2)
-	    {
+	        } 
+	    else if (name == 2);
+	        {
 	       System.out.println("Hi, "+ upperCaseName + ",its nice to meet you on this beautiful day." );
 		}
-        else 
-        { 
+             else 
+                { 
 	       System.out.println("Hi,its nice to meet you on this beautiful day." );
-        }
+                }
 	       
     } 
  }
